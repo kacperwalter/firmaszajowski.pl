cf41778f606708f7957be4a5fcb1aea0795e5d3a
Kacper Walter <kacperwalter@192.168.1.60>
2024-02-11 13:52:20 +0100

    Add commented legacy code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Homepage from "@/app/modules/Homepage/Homepage"
 
 import "@/app/common/styles/colors.scss"
+import "@/app/common/styles/fonts.scss"
+import "@/app/common/styles/globals.scss"
 
 const Home = () => {
   return (
