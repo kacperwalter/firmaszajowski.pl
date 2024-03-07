77b787130d6cc0d9f922eea09566e2cc7a7d29bd
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-03-07 13:42:56 +0100

    Add Navigation refactoring

--- app/common/components/organisms/Navbar/Navigation.tsx
+++ app/common/components/organisms/Navbar/Navigation.tsx
@@ -6,59 +6,43 @@ import Link from "next/link"
 import { usePathname } from 'next/navigation'
 import arrowDown from "@/public/icons/arrow-down.svg"
 import Dropdown from "@/app/common/components/molecules/Dropdown/Dropdown"
+import { navigationItems } from "@/app/data/navigationItems"
 
 const Navigation = () => {
   const pathname = usePathname()
-  
+
   const [isDropdownVisible, setIsDropdownVisible] = useState(false)
 
-  const setIsDropdownVisibleHandler = () => {
+  const toggleDropdownVisibility = () => {
     setIsDropdownVisible(!isDropdownVisible)
   }
 
   return (
     <ul className="navbar__navigation">
-      <li
-        className={`navbar__link ${pathname === "/inwestycje" ? "navbar__link--active" : ""}`}
-        onClick={setIsDropdownVisibleHandler}
-      >
-        <p
-          className="navbar__link-title"
-        >
-          Inwestycje
-          
-          <Image
-            src={arrowDown}
-            alt="arrow-down"
-            width={20}
-            height={20}
-          />
-        </p>
-
-        <Dropdown isVisible={isDropdownVisible} />
-      </li>
-
-      <li
-        className={`navbar__link ${pathname === "/o-firmie" ? "navbar__link--active" : ""}`}
-      >
-        <Link 
-          href="/o-firmie"
-          className="navbar__link-navlink"
-        >
-          O firmie
-        </Link>
-      </li>
-
-      <li 
-        className={`navbar__link ${pathname === "/kontakt" ? "navbar__link--active" : ""}`}
-      >
-        <Link
-          href="/kontakt"
-          className="navbar__link-navlink"
-        >
-          Kontakt
-        </Link>
-      </li>
+      {navigationItems.map(({ path, label, isDropdown }) => (
+        isDropdown ? (
+          <li
+            key={path}
+            className={`navbar__link ${pathname === path ? "navbar__link--active" : ""}`}
+            onClick={toggleDropdownVisibility}
+          >
+            <p className="navbar__link-title">
+              {label}
+              <Image src={arrowDown} alt="arrow-down" width={20} height={20} />
+            </p>
+            <Dropdown isVisible={isDropdownVisible} />
+          </li>
+        ) : (
+          <li
+            key={path}
+            className={`navbar__link ${pathname === path ? "navbar__link--active" : ""}`}
+          >
+            <Link href={path} className="navbar__link-navlink">
+              {label}
+            </Link>
+          </li>
+        )
+      ))}
     </ul>
   )
 }
---
+++ app/data/navigationItems.ts
@@ -0,0 +1,17 @@
+export const navigationItems = [
+    {
+      path: "/inwestycje",
+      label: "Inwestycje",
+      isDropdown: true,
+    },
+    {
+      path: "/o-firmie",
+      label: "O firmie",
+      isDropdown: false,
+    },
+    {
+      path: "/kontakt",
+      label: "Kontakt",
+      isDropdown: false,
+    },
+  ]
