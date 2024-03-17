03fa39b6a04605150bdde4942d261c923d0e8a22
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-03-17 19:43:40 +0100

    Add proper transition for button

--- app/common/components/atoms/ActionButton/ActionButton.tsx
+++ app/common/components/atoms/ActionButton/ActionButton.tsx
@@ -2,11 +2,11 @@ import "./ActionButton.scss"
 
 const ActionButton = () => {
   return (
-    <button className="action-button">
+    <a href="#" className="action-button">
       <svg className="action-button__arrow" width="14" height="20" viewBox="0 0 14 20" xmlns="http://www.w3.org/2000/svg">
         <path d="M14 13L12.59 11.59L8 16.17V0H6V16.17L1.41 11.58L0 13L7 20L14 13Z" fill="black"/>
       </svg>
-    </button>
+    </a>
   )
 }
 
