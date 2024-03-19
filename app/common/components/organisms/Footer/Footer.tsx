81590287beb106ed4679f0fb7b4179bca76678d5
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-03-19 11:20:10 +0100

    Add rest of HTML markup of footer

--- app/common/components/organisms/Footer/Footer.tsx
+++ app/common/components/organisms/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import BrandLogo from "@/app/common/components/atoms/BrandLogo/BrandLogo"
 import ContactInfo from "@/app/common/components/molecules/ContactInfo/ContactInfo"
+import { navigationItems } from "@/app/data/navigationItems"
+import { dropdownItems } from "@/app/data/dropdownItems"
 import "./Footer.scss"
 
 const Footer = () => {
@@ -10,7 +12,28 @@ const Footer = () => {
         <p>Firma Szajowski <br />ul. Słoneczna 23K, 64-800 Chodzież</p>
         <p>© 2024 Wszystkie prawa zastrzeżone</p>
       </div>
-      <nav className="footer__navigation"></nav>
+
+      <ul className="footer__navigation">
+        {navigationItems.map((item, index) => (
+          <li key={index}>
+            {item.isDropdown ? (
+              <>
+                <a href={item.path}>{item.label}</a>
+                <ul>
+                  {dropdownItems.map((dropdownItem, dropdownIndex) => (
+                    <li key={dropdownIndex}>
+                      <a href={dropdownItem.path}>{dropdownItem.title}</a>
+                    </li>
+                  ))}
+                </ul>
+              </>
+            ) : (
+              <a href={item.path}>{item.label}</a>
+            )}
+          </li>
+        ))}
+      </ul>
+      
       <ContactInfo
         phone="123 456 789"
         email="test@test.pl"
