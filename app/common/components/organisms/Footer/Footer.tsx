181f97679b8faaba38b9aebcac799f52c088a05b
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-03-19 11:04:26 +0100

    Add footer markup

--- app/common/components/organisms/Footer/Footer.tsx
+++ app/common/components/organisms/Footer/Footer.tsx
@@ -1,8 +1,21 @@
+import BrandLogo from "@/app/common/components/atoms/BrandLogo/BrandLogo"
+import ContactInfo from "@/app/common/components/molecules/ContactInfo/ContactInfo"
 import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <section>Footer</section>
+    <section className="footer">
+      <div className="footer__contact">
+        <BrandLogo />
+        <p>Firma Szajowski <br />ul. Słoneczna 23K, 64-800 Chodzież</p>
+        <p>© 2024 Wszystkie prawa zastrzeżone</p>
+      </div>
+      <nav className="footer__navigation"></nav>
+      <ContactInfo
+        phone="123 456 789"
+        email="test@test.pl"
+      />
+    </section>
   )
 }
 
