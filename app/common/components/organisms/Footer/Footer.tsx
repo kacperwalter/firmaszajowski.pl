b6620de63c9c997645403c4e5bccec107a1f7724
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-03-19 11:33:14 +0100

    Add more styling to footer

--- app/common/components/organisms/Footer/Footer.tsx
+++ app/common/components/organisms/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import BrandLogo from "@/app/common/components/atoms/BrandLogo/BrandLogo"
 import ContactInfo from "@/app/common/components/molecules/ContactInfo/ContactInfo"
+import Wrapper from "@/app/common/components/atoms/Wrapper/Wrapper"
 import { navigationItems } from "@/app/data/navigationItems"
 import { dropdownItems } from "@/app/data/dropdownItems"
 import "./Footer.scss"
@@ -7,37 +8,39 @@ import "./Footer.scss"
 const Footer = () => {
   return (
     <section className="footer">
-      <div className="footer__contact">
-        <BrandLogo />
-        <p>Firma Szajowski <br />ul. Słoneczna 23K, 64-800 Chodzież</p>
-        <p>© 2024 Wszystkie prawa zastrzeżone</p>
-      </div>
+      <Wrapper isWide>
+        <div className="footer__contact">
+          <BrandLogo />
+          <p>Firma Szajowski <br />ul. Słoneczna 23K, 64-800 Chodzież</p>
+          <p>© 2024 Wszystkie prawa zastrzeżone</p>
+        </div>
 
-      <ul className="footer__navigation">
-        {navigationItems.map((item, index) => (
-          <li key={index}>
-            {item.isDropdown ? (
-              <>
+        <ul className="footer__navigation">
+          {navigationItems.map((item, index) => (
+            <li key={index}>
+              {item.isDropdown ? (
+                <>
+                  <a href={item.path}>{item.label}</a>
+                  <ul>
+                    {dropdownItems.map((dropdownItem, dropdownIndex) => (
+                      <li key={dropdownIndex}>
+                        <a href={dropdownItem.path}>{dropdownItem.description}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              ) : (
                 <a href={item.path}>{item.label}</a>
-                <ul>
-                  {dropdownItems.map((dropdownItem, dropdownIndex) => (
-                    <li key={dropdownIndex}>
-                      <a href={dropdownItem.path}>{dropdownItem.title}</a>
-                    </li>
-                  ))}
-                </ul>
-              </>
-            ) : (
-              <a href={item.path}>{item.label}</a>
-            )}
-          </li>
-        ))}
-      </ul>
-      
-      <ContactInfo
-        phone="123 456 789"
-        email="test@test.pl"
-      />
+              )}
+            </li>
+          ))}
+        </ul>
+
+        <ContactInfo
+          phone="123 456 789"
+          email="test@test.pl"
+        />
+      </Wrapper>
     </section>
   )
 }
