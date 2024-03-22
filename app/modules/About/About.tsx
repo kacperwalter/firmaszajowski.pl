011447b2f96282e42aac8fa5440b664f740aa19e
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-03-22 13:39:39 +0100

    Add proper images

--- app/modules/About/About.tsx
+++ app/modules/About/About.tsx
@@ -3,13 +3,13 @@ import Details from "@/app/common/components/organisms/Details/Details"
 const heroData = {
   heading: "<strong>Kim jesteśmy?</strong>",
   caption: "Firma Szajowski jest firmą rodzinną, której działalność rozpoczęła się w 1982 roku z inicjatywy Ryszarda Szajowskiego. Z czasem, do załogi dołączyli jego synowie: Maciej i Paweł.<br><br>Dzisiaj Firma Szajowski oferuje nie tylko usługi budowlane, ale również budowę i sprzedaż mieszkań oraz domów.",
-  image: "images/example-image.png",
+  image: "images/o-firmie/planning.jpeg",
 }
 
 const detailsData1 = {
   heading: "<strong>Firma budowlana</strong>",
   caption: "Świadczymy usługi budowlane dedykowane powierzchniom mieszkalnym i dużym przestrzeniom użytkowym. Wykonujemy kompleksowe wykończenia wnętrz bloków mieszkalnych: tynki, posadzki, izolacje termiczne i akustyczne, szpachle, malowanie. <br> <br>  Nasi pracownicy pracują na materiałach budowlanych sprawdzonych, polskich producentów, a ich kompetencje potwierdza certyfikat autoryzowanej firmy wykonawczej.",
-  image: "images/example-image.png",
+  image: "image/o-firmie/planning-2.jpeg",
   icons: [
     "images/dolina-nidy-logo.png",
     "images/iso-logo.png",
@@ -19,7 +19,7 @@ const detailsData1 = {
 const detailsData2 = {
   heading: "<strong>Firma budowlana</strong>",
   caption: "Świadczymy usługi budowlane dedykowane powierzchniom mieszkalnym i dużym przestrzeniom użytkowym. Wykonujemy kompleksowe wykończenia wnętrz bloków mieszkalnych: tynki, posadzki, izolacje termiczne i akustyczne, szpachle, malowanie. <br><br>  Nasi pracownicy pracują na materiałach budowlanych sprawdzonych, polskich producentów, a ich kompetencje potwierdza certyfikat autoryzowanej firmy wykonawczej.",
-  image: "images/example-image.png",
+  image: "image/o-firmie/planning-2.jpeg",
 }
 
 const About = () => {
