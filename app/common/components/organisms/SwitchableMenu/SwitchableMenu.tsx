82908528e19c1f915dabab499d129d53ac517db4
kacperwalter <52107523+kacperwalter@users.noreply.github.com>
2024-03-23 15:12:04 +0100

    Add switchableMenuData

---
+++ app/common/components/organisms/SwitchableMenu/SwitchableMenu.tsx
@@ -0,0 +1,9 @@
+import "./SwitchableMenu.scss"
+
+const SwitchableMenu = () => {
+  return (
+    <div>SwitchableMenu</div>
+  )
+}
+
+export default SwitchableMenu
--- app/modules/Contact/Contact.tsx
+++ app/modules/Contact/Contact.tsx
@@ -1,8 +1,38 @@
-import ErrorPage from "@/app/common/components/molecules/ErrorPage/ErrorPage"
+import SwitchableMenu from "@/app/common/components/organisms/SwitchableMenu/SwitchableMenu"
+
+const switchableMenuData = {
+  heading: "Kontakt",
+  items: [
+    {
+      name: "Inwestycje mieszkaniowe",
+      heading: "Szajowski Inwestycje sp. z o.o.",
+      caption: "ul. Słoneczna 23K <br> 64-800 Chodzież <br> NIP: 6070089882",
+      phone: "+48 601 566 829",
+      email: "biuro@firmaszajowski.pl",
+    },
+    {
+      name: "Usługi budowlane",
+      heading: "Szajowski spółka cywilna <br> Maciej Szajowski Aleksandra Szajowska",
+      caption: "ul. Słoneczna 23K <br> 64-800 Chodzież <br> NIP: 6070032480 <br> Santader Bank Polska S.A. 10 1090 1317 0000 0001 0593 3970",
+      phone: "+48 601 566 829",
+      email: "biuro@firmaszajowski.pl",
+    },
+    {
+      name: "Materiały budowlane",
+      heading: "Firma Budowlana Paweł Szajowski",
+      caption: "ul. Słoneczna 23K <br> 64-800 Chodzież <br> NIP: 6070032480 <br> Santader Bank Polska S.A. 10 1090 1317 0000 0001 0593 3970",
+      phone: "+48 601 566 829",
+      email: "pawel@firmaszajowski.pl",
+    }
+  ]
+}
+
 
 const Contact = () => {
   return (
-    <ErrorPage type="500" />
+    <main>
+      <SwitchableMenu />
+    </main>
   )
 }
 
