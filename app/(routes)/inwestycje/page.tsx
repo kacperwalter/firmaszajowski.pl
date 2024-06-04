91e24bada10bf84c27e981904c680b584879653f
kacperwalter <kacwal@poczta.fm>
2024-06-04 21:35:38 +0200

    Add routing

---
+++ app/(routes)/inwestycje/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/app/common/components/organisms/Navbar/Navbar"
+import Footer from "@/app/common/components/organisms/Footer/Footer"
+import BckgRect from "@/app/common/components/atoms/BckgRect/BckgRect"
+
+const InwestycjePage = () => {
+  return (
+    <>
+      <BckgRect placement="center" />
+      <Navbar />
+      hehe
+      <Footer />
+    </>
+  )
+}
+
+export default InwestycjePage
