8a827b3b80a647341f56167ac2bd1f537d87700e
kacperwalter <kacwal@poczta.fm>
2024-06-13 15:50:03 +0200

    Add improved pots fetching

--- app/modules/Homepage/Homepage.tsx
+++ app/modules/Homepage/Homepage.tsx
@@ -13,44 +13,7 @@ const homepageHeroData = {
 const blogFeedData = {
   heading: "Nasze <br><strong>inwestycje mieszkaniowe</strong>",
   caption: "Zajmujemy się budową oraz sprzedażą mieszkań  i nowoczesnych domów szeregowych w spokojnych, komfortowych lokalizacjach. Nasze inwestycje położone są w Chodzieży i jej okolicach – staramy się, aby nasze budynki wpisywały się w bieżący krajobraz, oferując klientom dostęp do zieleni, bez szkodliwej ingerencji w środowisko naturalne.",
-  posts: [
-    {
-      image: {
-        src: "images/lesna-polana.jpg",
-        alt: "Leśna Polana - Domy wolnostojące",
-      },
-      heading: "Leśna Polana",
-      caption: "Domy wolnostojące",
-      slug: "lesna-polana-1",
-    },
-    {
-      image: {
-        src: "images/osiedle-sadowa.jpg",
-        alt: "Osiedle Sadowa - Nowoczesne domy dwulokalowe",
-      },
-      heading: "Osiedle Sadowa",
-      caption: "Nowoczesne domy dwulokalowe",
-      slug: "osiedle-sadowa",
-    },
-    {
-      image: {
-        src: "images/osiedle-gajowa.jpeg",
-        alt: "Osiedle Gajowa - Mieszkania",
-      },
-      heading: "Osiedle Gajowa",
-      caption: "Mieszkania",
-      slug: "osiedle-gajowa",
-    },
-    {
-      image: {
-        src: "images/lesna-polana-2.jpeg",
-        alt: "Leśna Polana - Domy bliźniacze",
-      },
-      heading: "Leśna Polana",
-      caption: "Domy bliźniacze",
-      slug: "lesna-polana-2",
-    },
-  ],
+  posts: [],
 }
 
 const CTAData = {
@@ -63,7 +26,18 @@ const CTAData = {
 
 const Home = async () => {
   const investments = await getInvestments()
-  console.log(investments)
+
+  const transformedInvestments = investments.map(investment => ({
+    image: {
+      src: investment.image.src,
+      alt: investment.image.alt,
+    },
+    heading: investment.name,
+    caption: investment.category,
+    slug: investment.slug,
+  }))
+
+  blogFeedData.posts = transformedInvestments
   
   return (
     <main>
