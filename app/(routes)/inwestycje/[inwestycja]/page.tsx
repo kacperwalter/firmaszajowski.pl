9944083285bde98061a75e47e678c51804090e8f
kacperwalter <kacwal@poczta.fm>
2024-06-13 18:55:37 +0200

    Add last refinements for displaying page

--- app/(routes)/inwestycje/[inwestycja]/page.tsx
+++ app/(routes)/inwestycje/[inwestycja]/page.tsx
@@ -1,5 +1,9 @@
 import { getInvestment } from "@/sanity/sanity-utils"
 
+import Navbar from "@/app/common/components/organisms/Navbar/Navbar"
+import Footer from "@/app/common/components/organisms/Footer/Footer"
+import BckgRect from "@/app/common/components/atoms/BckgRect/BckgRect"
+
 type Props = {
   params: { inwestycja: string }
 }
@@ -9,7 +13,18 @@ const Inwestycja = async ({ params }: Props) => {
   const investment = await getInvestment(slug)
 
   return (
-    <div>{investment.name}</div>
+    <>
+      <BckgRect placement="center" />
+
+      <Navbar />
+
+      <main>
+        <div>{investment.name}</div>
+
+      </main>
+
+      <Footer />
+    </>
   )
 }
 
