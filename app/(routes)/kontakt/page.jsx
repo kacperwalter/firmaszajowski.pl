30847a0d98774689435745c134369d3c3955fa9c
kacperwalter <kacwal@poczta.fm>
2024-06-16 23:33:30 +0200

    Mock styles

--- app/(routes)/kontakt/page.jsx
+++ app/(routes)/kontakt/page.jsx
@@ -3,6 +3,14 @@ import Footer from "@/app/common/components/organisms/Footer/Footer"
 import Contact from "@/app/modules/Contact/Contact"
 import BckgRect from "@/app/common/components/atoms/BckgRect/BckgRect"
 
+// TODO temporarly they have to be imported there, before single inwestycja is a module
+import "@/app/common/styles/colors.scss"
+import "@/app/common/styles/fonts.scss"
+import "@/app/common/styles/globals.scss"
+import "@/app/common/styles/spacings.scss"
+import "@/app/common/styles/utils/is-hidden.scss"
+// TODO remove these imports then
+
 // TODO CMS
 export const metadata = {
   title: "Firma Szajowski - Kontakt",
--- app/(routes)/o-firmie/page.jsx
+++ app/(routes)/o-firmie/page.jsx
@@ -3,6 +3,14 @@ import Navbar from "@/app/common/components/organisms/Navbar/Navbar"
 import Footer from "@/app/common/components/organisms/Footer/Footer"
 import About from "@/app/modules/About/About"
 
+// TODO temporarly they have to be imported there, before single inwestycja is a module
+import "@/app/common/styles/colors.scss"
+import "@/app/common/styles/fonts.scss"
+import "@/app/common/styles/globals.scss"
+import "@/app/common/styles/spacings.scss"
+import "@/app/common/styles/utils/is-hidden.scss"
+// TODO remove these imports then
+
 // TODO CMS
 export const metadata = {
   title: "Firma Szajowski - O firmie",
