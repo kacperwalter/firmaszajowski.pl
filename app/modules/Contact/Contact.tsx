69abf47c29e5a51859eac66d72b76723a74f922c
kacperwalter <kacwal@poczta.fm>
2024-09-05 15:20:34 +0200

    Fix for fetching data

--- app/modules/Contact/Contact.tsx
+++ app/modules/Contact/Contact.tsx
@@ -1,12 +1,31 @@
+// @ts-nocheck
+'use client'
+
+import { useState, useEffect } from "react"
 import SwitchableMenu from "@/app/common/components/organisms/SwitchableMenu/SwitchableMenu"
 import { getContactData } from "@/sanity/sanity-utils"
 
-const Contact = async () => {
-  const switchableMenuData = await getContactData()
+const Contact = () => {
+  const [switchableMenuData, setSwitchableMenuData] = useState(null)
+
+  useEffect(() => {
+    const fetchSwitchableMenuData = async () => {
+      try {
+        const data = await getContactData()
+        setSwitchableMenuData(data)
+      } catch (error) {
+        console.error("Failed to fetch switchable menu data:", error)
+      }
+    }
+
+    fetchSwitchableMenuData()
+  }, [])
 
   return (
     <main>
-      <SwitchableMenu content={switchableMenuData}/>
+      {switchableMenuData && (
+        <SwitchableMenu content={switchableMenuData} />
+      )}
     </main>
   )
 }
